747fe429225dfb806ea9c066cc944bb39ab1a376
신지훈 <rec8730@gmail.com>
2025-03-02 19:17:42 +0900

    protected login and user.id return add

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -14,29 +14,49 @@ export const authOptions = {
       async authorize(
         credentials: Record<'email' | 'password', string> | undefined,
       ) {
-        // 사용자 인증 로직
+        // 사용자 로그인
         const res = await axios.post(`${API_URL}/api/login`, {
           email: credentials?.email,
           password: credentials?.password,
         });
 
+        // 사용자 jwt 인증
+        const protectedRes = await axios.get(`${API_URL}/api/protected`, {
+          headers: {
+            Authorization: res?.data.token,
+          },
+        });
+
         // 로그인 성공 시 next-auth 에서는 기본적으로 email, name, image 만 반환 해줌
-        if (res?.data.token) {
-          return {
-            id: res.data.id, // 반환 하지 않음
+        if (protectedRes) {
+          const user = {
+            id: res.data.id,
             email: res.data.email,
             name: res.data.name,
             image: res.data.image,
           };
+          return user;
         } else {
           return null;
         }
       },
     }),
   ],
-  session: {
-    maxAge: 60 * 60 * 24 * 30, // 세션 만료시간 Default 30일
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id; // user.id를 JWT 토큰에 포함
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id;
+      }
+      return session;
+    },
   },
+  expires: 60 * 60 * 24 * 30, // 토큰 만료시간 Default 30일
 };
 
 const handler = NextAuth(authOptions);
